552c7b2005715e7f703fd4b08ab78ad9f7437ba0
Ali Elouafiq <ali@ali.ma>
2015-09-17 13:11:35 +0100

    adding a testing utility

---
+++ test.py
@@ -0,0 +1,6 @@
+import random
+import names
+meta_data = lambda x : {'comment_count':random.randrange(x),'share_count':random.randrange(x),'like_count':random.randrange(x),'time_stamp': random.randrange(x*1000),'url':'http://'+names.get_last_name()+'.com'}
+
+my_meta = 
+testing_meta = lambda x: map()
