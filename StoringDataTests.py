622e9457c92b79935734cc9c94c040b2a1da55e7
Ali Elouafiq <ali@ali.ma>
2015-09-18 15:38:34 +0100

    changing testing system to py.test

--- StoringDataTests.py
+++ StoringDataTests.py
@@ -1,4 +1,4 @@
-import random, names, unittest
+import random, names
 import mongo
 from util import *
 
@@ -14,53 +14,37 @@ meta_data = lambda link : {
 ramdom_metadata = lambda links: map(meta_data, links )
 
 
-class StoringDataTests(unittest.TestCase):
-
-    SAMPLE_SIZE = 20
-    SAMPLE_LINKS =  random_links(SAMPLE_SIZE)
-    SAMPLE_METADATA = ramdom_metadata(SAMPLE_LINKS)
-          
-
-    def setUp(self):
-        mongo.LinksCollection.delete_many({})
-        mongo.MetricsCollection.delete_many({})
-        #print self.SAMPLE_METADATA
 
 
-    def test_saving_links_in_mongo(self):
+SAMPLE_SIZE = 20
+SAMPLE_LINKS =  random_links(SAMPLE_SIZE)
+SAMPLE_METADATA = ramdom_metadata(SAMPLE_LINKS)
+mongo.LinksCollection.delete_many({})
+mongo.MetricsCollection.delete_many({})          
 
-        #if flush == True: mongo._db.links.delete_many({})
-        # The function we are testing
-        
 
-        mongo.save_links(self.SAMPLE_LINKS)
-        cursor = mongo.LinksCollection.find()
-        self.fetched_links = [ line['url'] for line in cursor]
+def test_saving_links_in_mongo():
+    print 'lol'
+    mongo.save_links(SAMPLE_LINKS)
+    cursor = mongo.LinksCollection.find()
+    fetched_links = [ line['url'] for line in cursor]
 
-        for line in self.SAMPLE_LINKS:
-            self.assertIn(line, self.fetched_links, msg='checking : '+line)
-        
-
-    def test_facebook_meta_data(self):
-        
-        #the function we are testing
-        mongo.store_metrics_data(self.SAMPLE_METADATA)
-        fetched_data = fetch(mongo.LinksCollection)
-        #building the comparator to compare with our actual data
-        comparator = dict()
-        for m in fetched_data: 
-            url = m['url']
-            comparator[url] = clean(m,['url','stats.fb_stats.time_stamp'])
-            m['url'] = url
-        
-        for line in fetched_data:
-            self.assertEqual(line['stats']['fb_stats'], comparator[line['url']]['stats']['fb_stats'] )
-            
+    for line in SAMPLE_LINKS:
+       assert line in fetched_links
     
 
-if __name__ == '__main__':
-    unittest.main()
-    print self.SAMPLE_LINKS
-
-
+def test_facebook_meta_data():
+    print 'saving'
+    #the function we are testing
+    mongo.store_metrics_data(SAMPLE_METADATA)
+    fetched_data = fetch(mongo.LinksCollection)
+    #building the comparator to compare with our actual data
+    comparator = dict()
+    for m in fetched_data: 
+        url = m['url']
+        comparator[url] = clean(m,['url','stats.fb_stats.time_stamp'])
+        m['url'] = url
+    
+    for line in fetched_data:
+        assert line['stats']['fb_stats'] == comparator[line['url']]['stats']['fb_stats'] 
 
