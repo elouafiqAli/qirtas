8f9709db6c8f19f1dc1d62e70fcd07e673f77da5
Ali Elouafiq <ali@ali.ma>
2015-10-11 12:06:28 +0100

    supporting paging for graphAPI facebook

--- testing.py
+++ testing.py
@@ -4,72 +4,56 @@ from urlparse import urlparse, parse_qs
 
 
 users = ['me']
-test_token = 'CAACEdEose0cBALWl8S9PRAzTZAEIyvvbwtDO0fueDuHbmbVZBGckgivpSZCxyZAJYBT3BZCdXimCS5sDCTuqEWBWL3MHe30BSFxL8n87YJ7lZAf7kDacRmN2JCucvvwjIA43EWwQbedEghF7ZBJVHtfZA4z0pVwsZBfpbA6HFif1PbR32MBZArhVQraBCuGR1ZCtZAFc7gTYuOxkNwZDZD'
-def get_graph(oauth_token):
-	return facebook.GraphAPI(access_token=oauth_token)
-
-def run_test():
-	_graph = get_graph(test_token)
-	all_posts = []
-
-	#initilaizing the request --> /me/posts
-	_a = {
-		'id':'me',
-		'edge':'posts',
-		'args': {}
-	}
-	i=0
-	while(True):
-		try:
-			#getting the list of posts
-			response = _graph.get_connections(_a['id'],_a['edge'],_a['args'])
-			#storing the posts
-			all_posts.extend(response['data'])
-
-			"""
-				Getting the next url if available, if not it throws an exception to exit the loop
-				We parse the next url to transform in into a valide get_connections request
+test_token = 'CAACEdEose0cBAPpZCp4PZA9lQSRqJQSsk4glIDCpfq0S4LbQTLVKHeuoGIZBQlVFcguuEniWpZAahUWoXdoaOUnLfrrDezSZCDRSpIE7TcXRq22f603wZAceGTE8g93hGK6NvOEImNYTCzdKPZAstNz6AVmSV5ny1qbaToItIE4vJUOXbPvoQ6yplGrQ9biJ57fkxi6q4tCCQZDZD'
 
 
-			"""
+_fb_api_version = '2.4'
 
-			#the next url
-			next = response['paging']['next']
-			#we parse the url to 
-			parsed_url = urlparse(next)		
-			get_arguments =  parse_qs(parsed_url.query)
-			print parsed_url
-			print '--------'
-			print get_arguments
-			for key in get_arguments: 
-				print get_arguments[key]
-				get_arguments[key] = get_arguments[key][0]
-			del get_arguments['access_token']
-
-			"""
-				The url:
-				/v2.4/{id}/{edge}/access_token?={access_token}&arguments=value&cursor=value....
-					the cursors are passed as parameters
-
-			"""
-			if i > 8 :
-				break 
-			_a = {
-
-				'id': parsed_url.path.split('/')[2],
-				'edge': parsed_url.path.split('/')[3],
-				'args' : get_arguments
-			}
-			i = i + 1
-
-
-		except KeyError:
-			break
-
-
+def graph_request(url):
+	path = url.split(_fb_api_version)[1].split('/') if len(url.split('/v2')) > 1 else url.split('/')
 	
+	node = path[1] 
+	edge = path[2].split('?')[0]
+	params = parse_qs(urlparse(url).query) if len(path) > 2 else {}
+	
+	try:
+		del params['access_token']
+	except:
+		pass
 
-	return all_posts
-if __name__ == "__main__":
-	oauth_token = sys.argv[1]  if len(sys.argv) > 1 else token.get_token()
-	_graph = get_graph(oauth_token)
+	request =  { 'node':  node, 'edge' : edge, 	'params' : params }
+	return request
+
+
+class bennay:
+	def __init__(self,oauth_token,api_version=_fb_api_version):
+		self.graph = facebook.GraphAPI(access_token=oauth_token,version = api_version)
+		self.all_posts = []
+
+	def paging(self,path):
+		request = graph_request(path)
+		
+		while True:
+			try:
+				print request
+				response = self.graph.get_connections(request['node'],request['edge'],request['params'])
+				yield response
+				request = graph_request(response['paging']['next'])
+
+			except KeyError:
+				raise StopIteration
+
+	def get_posts(self):
+		all_posts = []
+		posts = self.paging('/me/posts')
+		while True:
+			try:
+				all_posts.extend(posts.next()['data'])
+			except StopIteration:
+				return all_posts
+
+# --- used for interactive shell testing
+def tee():
+	reload(testing)
+	ben = testing.bennay(testing.test_token)
+	return ben.get_posts()
