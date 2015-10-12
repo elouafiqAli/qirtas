22bd56ae97d77d150db005e601dc5019c49d0c5d
Ali Elouafiq <ali@ali.ma>
2015-10-12 10:28:03 +0100

    changing the paging of the requests

--- testing.py
+++ testing.py
@@ -1,59 +1,24 @@
 import token, facebook
 import sys
 from urlparse import urlparse, parse_qs
-
+from multiprocessing import Pool
 
 users = ['me']
-test_token = 'CAACEdEose0cBAPpZCp4PZA9lQSRqJQSsk4glIDCpfq0S4LbQTLVKHeuoGIZBQlVFcguuEniWpZAahUWoXdoaOUnLfrrDezSZCDRSpIE7TcXRq22f603wZAceGTE8g93hGK6NvOEImNYTCzdKPZAstNz6AVmSV5ny1qbaToItIE4vJUOXbPvoQ6yplGrQ9biJ57fkxi6q4tCCQZDZD'
+test_token = 'CAACEdEose0cBAEONElBEbR6MpnNE5fXprkpfDzakUMimIAbqu3NCc5C6SCLFZC6uySXgVdZBVYmGmp5o97a66eB9Sbwo0mcqnpFvZC3ZBDRHiYFBtFWka1n4e9U8NyaZAl35vcUpU0kZB62xZCZBImZC5SzHf8XiznrLCwZAI0PQB8bZAkZBkgEcyuNrgLzxIERRNv0FfSZAuIZBrJoAZDZD'
 
 
 _fb_api_version = '2.4'
-
-def graph_request(url):
-	path = url.split(_fb_api_version)[1].split('/') if len(url.split('/v2')) > 1 else url.split('/')
-	
-	node = path[1] 
-	edge = path[2].split('?')[0]
-	params = parse_qs(urlparse(url).query) if len(path) > 2 else {}
-	
-	try:
-		del params['access_token']
-	except:
-		pass
-
-	request =  { 'node':  node, 'edge' : edge, 	'params' : params }
-	return request
+graph = facebook.GraphAPI(test_token,version =_fb_api_version )
 
 
-class bennay:
-	def __init__(self,oauth_token,api_version=_fb_api_version):
-		self.graph = facebook.GraphAPI(access_token=oauth_token,version = api_version)
-		self.all_posts = []
-
-	def paging(self,path):
-		request = graph_request(path)
+def get_posts():
+	all_posts = []
+	posts = graph.get_connections_paging('me','posts',{})
+	for post_list in posts: 
+		all_posts.extend(post_list['data'])
 		
-		while True:
-			try:
-				print request
-				response = self.graph.get_connections(request['node'],request['edge'],request['params'])
-				yield response
-				request = graph_request(response['paging']['next'])
+	return all_posts
 
-			except KeyError:
-				raise StopIteration
-
-	def get_posts(self):
-		all_posts = []
-		posts = self.paging('/me/posts')
-		while True:
-			try:
-				all_posts.extend(posts.next()['data'])
-			except StopIteration:
-				return all_posts
-
-# --- used for interactive shell testing
-def tee():
-	reload(testing)
-	ben = testing.bennay(testing.test_token)
-	return ben.get_posts()
+def get_post(id):
+	shares = graph.get_object(id,fields='shares')
+	likes = graph.get_conne
