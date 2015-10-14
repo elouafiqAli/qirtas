a2acd2904e26d7190cdbeccaea816da823d23e41
Ali Elouafiq <ali@ali.ma>
2015-10-14 12:36:50 +0100

    proper testing for Facebook Connection

--- testing.py
+++ testing.py
@@ -1,14 +1,49 @@
 import token
 import facebook_dialy as facebook
+import facebook as facebook_
+import re, testdata
 import sys
 from util import DotNotation, objectify
 from collections import namedtuple
 
 users = ['me']
-test_token = 'CAACEdEose0cBAK4uR2OwqbKYIWthO5QizkffHHBZC14py3ueNx2JoocPQupQKNaiMkDZCTTEHZByecmgsyB4SZAZCbMSZC4ZAP6ZCZCUQgDc026hRS6hZAlurI81LNLp4XiOnQx6KrP788GByr73HvZAu20I11hkO2TwcZAeo899gOovgVZAMZBcKdfGZChXmuZBeckZCQ3yXZCtdrflrP9gZDZD'
+test_token = 'CAACEdEose0cBAD4is3LUoiGCysrqFF4ljDZCTXPAuyVtQ4R6zMCdppLNs2IbaegmYtWFIXGSzEha4JEJGt49ullBYYeuxZAZCLTHTXEjlINIZBFHfGTv9BIL04ZCQRkpTjf4eFgAPnQgHy34ZCapD5VFwvfFZCNDkrFoNDZAjPGZBhgyrUVHARqi7ySpwFXK2IhJGfcUWLGCcjwZDZD'
+
+class TestingFacebook(facebook.GraphAPI):
+
+    def cached_paths(self,path):
+        cache = {
+                "%s/%s/%s" % (self.version, '(.*)','posts') : testdata.test_feed,
+                "%s/%s/%s" % (self.version, '(.*)','comments') : testdata.test_comments,
+            }[path]
+
+        for test_path in cache:
+            if re.match(test_path,path):
+                return cache[test_path]
+            raise Exception
+
+    def request(self,*args, **kargs):
+        path = args[0]
+        def cached_paths(self,path):
+            cache = {
+                    "%s/%s/%s" % (self.version, '(.*)','posts') : testdata.test_feed,
+                    "%s/%s/%s" % (self.version, '(.*)','comments') : testdata.test_comment
+            }
+
+            for test_path in cache:
+                if re.match(test_path,path):
+                    return cache[test_path]
+            raise Exception(path)
+
+        try:
+            result = cached_paths(path)
+            return result
+        except Exception:
+            return super(TestingFacebook,self).request(path,kargs)
 
 _fb_api_version = '2.4'
-graph = facebook.GraphAPI(test_token)
+graph = TestingFacebook(test_token)
+
 
 
 def get_posts():
@@ -21,19 +56,6 @@ def get_posts():
     for post_ in all_posts[:10]:
         post = Post(post_)
         print post.get_counters()
-
-    return all_posts
-
-
-def test_cnt():
-    all_posts = []
-    posts = graph.get_pages('me', 'posts')
-    for post_list in posts:
-        for post_ in post_list.data:
-            post = Post(post_)
-            print dir(post)
-            print post.get_counters()
-        break
 
     return all_posts
 
@@ -58,26 +80,26 @@ class Post:
     def __str__(self):
         return self.id
 
-    @objectify
+    #@objectify
     def get_contributors(self):
 
         comments, commenters, tagged, likers = [], [], [], []
 
         def get_likers():
             likers = {}
-            like_pages = graph.get_pages(id, 'likes')
+            like_pages = graph.get_pages(self.id, 'likes')
             for like_list in like_pages:
                 likers.extend(like_list.data)
             return likers
 
         comments_pages = graph.get_pages(
-            id, 'comments', fields='like_count,message_tags,created_time,from')
+            self.id, 'comments', fields='like_count,message_tags,created_time,from')
 
         for comments_list in comments_pages:
             for comment in comments_list.data:
                 # add comment to the list
-                comments.extend(comment)
-
+                comments.append(comment)
+                print comment
                 contributor = comment.from_
                 contributor.like_count = comment.like_count
                 contributor.created_time = comment.created_time
@@ -91,6 +113,26 @@ class Post:
 
         contributors = {'likers': get_likers(),
                         'commenters': commenters,
+                        'comments': comments,
                         'tagged': tagged
                         }
         return contributors
+
+
+def test_cnt():
+    all_posts = []
+    posts = graph.get_pages('me', 'posts')
+    for post_list in posts:
+        for post_ in post_list.data:
+            post = Post(post_)
+            #print dir(post)
+            print post.get_contributors()
+
+        break
+
+    return all_posts
+
+
+if __name__ == '__main__':
+
+    print test_cnt()
